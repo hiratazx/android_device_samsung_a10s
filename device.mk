b0c420ae947a33e1399bc2685b21acae434e893d
nift4 <nift4@protonmail.com>
2022-09-16 17:51:36 +0100

    a10s: libshims: Shim beanpod keymaster
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>
    Signed-off-by: Raka Prasetya Pratama <itzkaguya@yukiprjkt.my.id>

--- device.mk
+++ device.mk
@@ -267,7 +267,8 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Shims
 PRODUCT_PACKAGES += \
-    libshim_audio
+    libshim_audio \
+    libshim_beanpod
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
