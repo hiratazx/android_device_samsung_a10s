f8bc0bbeda3b5ac2215538470e5c2e6fb1884ee2
Michael Bestas <mkbestas@lineageos.org>
2022-10-03 03:49:23 +0300

    fixup! a10s: Build fastboot custom HAL
    Change-Id: If8b60462306f2f92789f57749e9c6dcb81e2d096
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>
    Signed-off-by: Raka Prasetya Pratama <itzkaguya@yukiprjkt.my.id>

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-custom \
+    android.hardware.fastboot@1.1-impl.custom \
     fastbootd
 
 # Health
