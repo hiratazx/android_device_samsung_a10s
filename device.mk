6e841b3589b3b2635cdef80ba1cda6b34aa75907
Raka Prasetya Pratama <itzkaguya@yukiprjkt.my.id>
2025-05-12 10:18:40 +0000

    a10s: build DRM libs
    Signed-off-by: Raka Prasetya Pratama <itzkaguya@yukiprjkt.my.id>

--- device.mk
+++ device.mk
@@ -97,6 +97,16 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
+    libmockdrmcryptoplugin \
+    libdrm.vendor
+
 # LPM
 PRODUCT_PACKAGES += \
     libsuspend 
